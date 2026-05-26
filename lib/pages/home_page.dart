9cca3a8a557e3c12902d0a6d3aed132eaa547e81
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-26 21:25:54 +0600

    Home page played music shown

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,6 +15,9 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     final songs = ref.watch(playlistProvider).toList();
+    final notifier = ref.watch(playlistProvider.notifier);
+    final index = notifier.currentIndex;
+    final playing = notifier.isPlaying;
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       appBar: AppBar(title: Text('P L A Y L I S T'), centerTitle: true),
@@ -41,6 +44,72 @@ class _HomePageState extends ConsumerState<HomePage> {
           );
         },
       ),
+      bottomNavigationBar: (index != null || songs.isNotEmpty)
+          ? GestureDetector(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SongPage()),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                height: 70,
+                margin: EdgeInsets.all(12),
+                child: Row(
+                  children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(16),
+                        bottomLeft: Radius.circular(16),
+                      ),
+                      child: Image.asset(songs[index!].albumArtImagePath),
+                    ),
+
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Now playing...',
+                              style: TextStyle(fontSize: 10),
+                            ),
+                            Text(
+                              songs[index].songName,
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                            Text(songs[index].artistName),
+                          ],
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        notifier.playPrevious();
+                      },
+                      icon: Icon(Icons.skip_previous),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        notifier.playOrPause();
+                      },
+                      icon: Icon(playing ? Icons.pause : Icons.play_arrow),
+                    ),
+
+                    IconButton(
+                      onPressed: () => notifier.playNext(),
+                      icon: Icon(Icons.skip_next),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : null,
     );
   }
 }
